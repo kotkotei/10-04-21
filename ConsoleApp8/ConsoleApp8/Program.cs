bc36c5d369c6f92f008c948d9026335d3abf4988
kotkotei <kotkotei@yandex.ru>
2021-04-10 09:48:47 +0700

    10 04 21 мишуля чучуля

---
+++ ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1.14B");
+            Console.WriteLine("Введите число");
+            double a = Convert.ToDouble(Console.ReadLine());
+            double b2 = -7.5 * Math.Pow(a, 2);
+            Console.WriteLine("ответ:" + b2);
+            Console.ReadKey();
+
+
+
+
+            Console.WriteLine("1.14 54" +
+                ");
+            Console.WriteLine("Введите число x");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите число y");
+            double y = Convert.ToDouble(Console.ReadLine());
+            double xy = -5 * Math.Sqrt(x + Math.Sqrt(y));
+            Console.WriteLine("Ответ" + xy);
+           Console.ReadKey();
+
+            
+
+
+
+        }
+    }
+}
